6080b092f2fa1a0a6dbff3e1d81c5a35d65186ae
luluxing <luluxing1988@gmail.com>
2025-02-26 21:07:17 -0500

    Done range query and duplicated value on lippxy

--- src/core/two_lipp.h
+++ src/core/two_lipp.h
@@ -49,17 +49,98 @@ class LIPP_XY {
         y_num++;
       }
     }
+
     lipp_y.bulk_load(y_coords.data(), y_num);
   }
 
   int knn_query(const Point<T>& key, int k, Point<T>* results, double* distances) {}
-  int range_query(const Point<T>& lower, const Point<T>& upper, Point<T>* results) const {}
+
+  int range_query(const Point<T>& lower, const Point<T>& upper, Point<T>* results) const {
+    // We only query the smaller range of x or y
+    T x_span = upper.x - lower.x;
+    T y_span = upper.y - lower.y;
+    if (x_span < y_span) {
+      // Query x
+      std::vector<uint64_t> x_result = lipp_x.rangeQuery(lower.x, upper.x);
+      // Filter by y
+      for (auto& x_ind : x_result) {
+        auto& pt = pts_sort_x[x_ind];
+        for (int xi = x_ind; xi < pts_sort_x.size(); ++xi) {
+          auto temp = pts_sort_x[xi];
+          if (pt.x != temp.x) break;
+          if (pt.y >= lower.y && pt.y <= upper.y) {
+            results->push_back(temp);
+          } else if (pt.y > upper.y) {
+            break;
+          }
+        }
+      }
+      return results->size();
+    } else {
+      // Query y
+      std::vector<uint64_t> y_result = lipp_y.rangeQuery(lower.y, upper.y);
+      // Filter by x
+      for (auto& y_ind : y_result) {
+        auto& pt = pts_sort_y[y_ind];
+        for (int yi = y_ind; yi < pts_sort_y.size(); ++yi) {
+          auto temp = pts_sort_y[yi];
+          if (pt.y != temp.y) break;
+          if (pt.x >= lower.x && pt.x <= upper.x) {
+            results->push_back(temp);
+          } else if (pt.x > upper.x) {
+            break;
+          }
+        }
+      }
+      return results->size();
+    }
+  }
+
   void rangeQuery(const Point<T>& min_key,
-    const Point<T>& max_key,
-    std::vector<Point<T>>& result) {}
+                  const Point<T>& max_key,
+                  std::vector<Point<T>>& result) {
+    // We only query the smaller range of x or y
+    T x_span = max_key.x - min_key.x;
+    T y_span = max_key.y - min_key.y;
+    if (x_span < y_span) {
+      // Query x
+      std::vector<std::pair<T, uint64_t>> x_result = lipp_x.rangeQuery(min_key.x, max_key.x);
+      // Filter by y
+      for (auto& x_ind : x_result) {
+        auto& pt = pts_sort_x[x_ind.second];
+        for (int xi = x_ind.second; xi < pts_sort_x.size(); ++xi) {
+          auto temp = pts_sort_x[xi];
+          if (pt.x != temp.x) break;
+          if (pt.y >= min_key.y && pt.y <= max_key.y) {
+            result.push_back(temp);
+          } else if (pt.y > max_key.y) {
+            break;
+          }
+        }
+      }
+    } else {
+      // Query y
+      std::vector<std::pair<T,uint64_t>> y_result = lipp_y.rangeQuery(min_key.y, max_key.y);
+      // Filter by x
+      for (auto& y_ind : y_result) {
+        auto& pt = pts_sort_y[y_ind.second];
+        for (int yi = y_ind.second; yi < pts_sort_y.size(); ++yi) {
+          auto temp = pts_sort_y[yi];
+          if (pt.y != temp.y) break;
+          if (pt.x >= min_key.x && pt.x <= max_key.x) {
+            result.push_back(temp);
+          } else if (pt.x > max_key.x) {
+            break;
+          }
+        }
+      }
+    }
+  }
 
   void insert(const Point<T>& key) {
-    
+    // Not supported
+    fprintf(stderr, "Insert not supported in LIPP_XY\n");
+
   }
 
   bool exists(const Point<T>& key) {
--- test/test_lippxy.cc
+++ test/test_lippxy.cc
@@ -6,7 +6,7 @@
 TEST(LIPPXYTest, BulkloadAndExist) {
   {
     LIPP_XY<int> mlipp;
-    int num = 100;
+    int num = 10000;
     std::vector<Point<int>> data;
     for (int i = 0; i < num; ++i) {
       data.push_back((Point<int>){i * 3, i * 4});
@@ -42,22 +42,99 @@ TEST(LIPPXYTest, BulkloadAndExist) {
   }
 }
 
-TEST(LIPPXYTest, InitialConstructionAndSingleInsertion) {
-  
-}
-
-TEST(LIPPXYTest, BulkloadingAndExistingData) {
-  
-}
-
-TEST(LIPPXYTest, NonExistingData) {
-  
-}
-
 TEST(LIPPXYTest, RangeQuery) {
-  
+  int num = 100000;
+  {
+    LIPP_XY<int> lipp;
+    std::vector<Point<int>> data;
+    int min_x = INT_MAX, min_y = INT_MAX, max_x = INT_MIN, max_y = INT_MIN;
+    for (int i = 0; i < num; ++i) {
+      data.push_back((Point<int>){rand(), rand()});
+      min_x = std::min(min_x, data[i].x);
+      min_y = std::min(min_y, data[i].y);
+      max_x = std::max(max_x, data[i].x);
+      max_y = std::max(max_y, data[i].y);
+    }
+    // Bulk load
+    lipp.bulk_load(data.data(), data.size());
+    // Range query
+    std::vector<Point<int>> result;
+    lipp.rangeQuery((Point<int>){min_x, min_y}, (Point<int>){max_x, max_y}, result);
+    EXPECT_EQ(result.size(), num);
+    // Sort both data and result and check if they are the same
+    std::sort(data.begin(), data.end());
+    std::sort(result.begin(), result.end());
+    for (int i = 0; i < num; ++i) {
+      EXPECT_EQ(data[i], result[i]);
+    }
+  }
+  {
+    LIPP_XY<double> mlipp;
+    std::vector<Point<double>> data;
+    double min_x = 1.0, min_y = 1.0, max_x = 0.0, max_y = 0.0;
+    for (int i = 0; i < num; ++i) {
+      data.push_back((Point<double>)
+        {rand() / static_cast<double>(RAND_MAX), rand() / static_cast<double>(RAND_MAX)});
+      min_x = std::min(min_x, data[i].x);
+      min_y = std::min(min_y, data[i].y);
+      max_x = std::max(max_x, data[i].x);
+      max_y = std::max(max_y, data[i].y);
+    }
+    // Bulk load
+    mlipp.bulk_load(data.data(), data.size());
+    // Range query
+    std::vector<Point<double>> result;
+    mlipp.rangeQuery((Point<double>){min_x, min_y}, (Point<double>){max_x, max_y}, result);
+    EXPECT_EQ(result.size(), num);
+    // Sort both data and result and check if they are the same
+    std::sort(data.begin(), data.end());
+    std::sort(result.begin(), result.end());
+    for (int i = 0; i < num; ++i) {
+      EXPECT_EQ(data[i], result[i]);
+    }
+  }
 }
 
 TEST(LIPPXYTest, DuplicateCoordinates) {
+  int num = 10;
+  {
+    LIPP_XY<int> lipp;
+    std::vector<Point<int>> data;
+    int min_x = INT_MAX, min_y = INT_MAX, max_x = INT_MIN, max_y = INT_MIN;
+    for (int i = 0; i < num; ++i) {
+      data.push_back((Point<int>){i+1, i+1});
+    }
+    data.push_back((Point<int>){1, 2});
+    data.push_back((Point<int>){1, 3});
 
+    // Bulk load
+    lipp.bulk_load(data.data(), data.size());
+
+    // Existence
+    EXPECT_TRUE(lipp.exists((Point<int>){1, 2}));
+    EXPECT_TRUE(lipp.exists((Point<int>){1, 3}));
+    EXPECT_FALSE(lipp.exists((Point<int>){1, 4}));
+
+    // Range query: skinny rectange
+    std::vector<Point<int>> result;
+    lipp.rangeQuery((Point<int>){0, 0}, (Point<int>){2, 3}, result);
+    EXPECT_EQ(result.size(), 4);
+    // Sort both data and result and check if they are the same
+    std::sort(data.begin(), data.end());
+    std::sort(result.begin(), result.end());
+    for (int i = 0; i < result.size(); ++i) {
+      EXPECT_EQ(data[i], result[i]);
+    }
+
+    // Range query: wide rectange
+    result.clear();
+    lipp.rangeQuery((Point<int>){0, 0}, (Point<int>){10, 3}, result);
+    EXPECT_EQ(result.size(), 5);
+    // Sort both data and result and check if they are the same
+    std::sort(data.begin(), data.end());
+    std::sort(result.begin(), result.end());
+    for (int i = 0; i < result.size(); ++i) {
+      EXPECT_EQ(data[i], result[i]);
+    }
+  }
 }
