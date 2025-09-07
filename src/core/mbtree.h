30f08176497f12d279e94121fe2dd96ed4ea379a
luluxing <luluxing1988@gmail.com>
2025-09-07 17:03:29 -0400

    Implement a single node bulkload and lookup

--- src/core/mbtree.h
+++ src/core/mbtree.h
@@ -1,13 +1,83 @@
 #ifndef __MBTREE_H__
 #define __MBTREE_H__
 
+#include <algorithm>
 #include "point.h"
+
+static const uint64_t PAGESIZE = 4*1024;
+
+struct Node {
+  bool is_leaf;
+  bool sort_by_x;
+  uint16_t count;
+};
+
+template <typename T>
+struct LeafNode : public Node {
+  static const uint16_t max_count = (PAGESIZE - sizeof(Node)) / sizeof(Point<T>);
+  Point<T> points[max_count];
+
+  LeafNode(bool sort_by_x) {
+    is_leaf = true;
+    this->sort_by_x = sort_by_x;
+    count = 0;
+  }
+
+  void load(Point<T>* vs, int begin, int num_keys) {
+    for (int i = 0; i < num_keys; i++) {
+      points[i] = vs[begin + i];
+    }
+    if (sort_by_x) {
+      qsort(points, num_keys, sizeof(Point<T>), &compare_x<T>);
+    } else {
+      qsort(points, num_keys, sizeof(Point<T>), &compare_y<T>);
+    }
+    count = num_keys;
+  }
+
+  T get_dim(const Point<T>& key) const {
+    return sort_by_x ? key.x : key.y;
+  }
+
+  bool exists(const Point<T>& key) const {
+    int lower = 0;
+    int upper = count;
+    do {
+      int mid = ((upper - lower) / 2) + lower;
+      if (get_dim(key) < get_dim(points[mid])) {
+        upper = mid;
+      } else if (get_dim(key) > get_dim(points[mid])) {
+        lower = mid+1;
+      } else {
+        return true;
+      }
+    } while (lower < upper);
+    return false;
+  }
+};
+
+template <typename T>
+struct InnerNode : public Node {
+  static const uint16_t max_count = (PAGESIZE - sizeof(Node)) / (sizeof(T) +sizeof(Node*));
+  T keys[max_count];
+  Node* children[max_count];
+
+  InnerNode(bool sort_by_x) {
+    this->sort_by_x = sort_by_x;
+    is_leaf = false;
+    count = 0;
+  }
+
+  Node* find_child(const Point<T>& key) const {
+    return children[0];
+  }
+};
 
 // Store the 2D points in the way of MLIPP_kd
 template <typename T>
 class MBTree {
  public:
-  MBTree() {}
+  MBTree() { root_ = nullptr; }
   ~MBTree() {}
 
   void insert(const Point<T>& key) {
@@ -15,6 +85,16 @@ class MBTree {
   }
 
   bool exists(const Point<T>& key) const {
+    Node* node = root_;
+    while (true) {
+      if (node->is_leaf) {
+        return static_cast<LeafNode<T>*>(node)->exists(key);
+      } else {
+        InnerNode<T>* inner_node = static_cast<InnerNode<T>*>(node);
+        node = inner_node->find_child(key);
+      }
+    }
+    return false;
 
   }
 
@@ -32,7 +112,16 @@ class MBTree {
 
   }
 
+  // Points are not initially sorted
   void bulk_load(Point<T>* vs, int num_keys) {
+    if (LeafNode<T>::max_count >= num_keys) {
+      root_ = new LeafNode<T>(true);
+      auto leaf_node = static_cast<LeafNode<T>*>(root_);
+      leaf_node->load(vs, 0, num_keys);
+    } else {
+      root_ = new InnerNode<T>(true);
+      
+    }
 
   }
 
@@ -57,6 +146,7 @@ class MBTree {
   }
 
  private:
+  Node* root_;
 };
 
 #endif // __MBTREE_H__
--- test/test_mbtree.cc
+++ test/test_mbtree.cc
@@ -6,3 +6,16 @@ TEST(MBTreeTest, Construction) {
   MBTree<int> mbtree;
   EXPECT_EQ(mbtree.index_size(), 0);
 }
+
+TEST(MBTreeTest, BulkLoadSingleLeaf) {
+  MBTree<int> mbtree;
+  int num = 10;
+  std::vector<Point<int>> data;
+  for (int i = 0; i < num; ++i) {
+    data.push_back((Point<int>){i, i});
+  }
+  mbtree.bulk_load(data.data(), data.size());
+  for (int i = 0; i < num; ++i) {
+    EXPECT_TRUE(mbtree.exists(data[i]));
+  }
+}
